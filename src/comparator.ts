2caf96cec58a48bb12a8f155ab56ee714d744b74
“Igor <imagdich@questrade.com>
2021-09-02 15:03:51 +0300

    update node modules

--- src/comparator.ts
+++ src/comparator.ts
@@ -31,29 +31,39 @@ export class Comparator {
         const png2: PNG = PNG.sync.read(file2Content);
 
         const result: number = this.comparePngImages(png1, png2, excludedAreas ?? [], diffFilePath);
+
         return result === 0;
     }
 
     static comparePngImages(img1: PNG, img2: PNG, excludedAreas: Area[], diffFilePath?: string): number {
         const { width: width1, height: height1 } = img1;
         const { width: width2, height: height2 } = img1;
-
+        const imageSizesDoNotMatch = height1 !== height2 || width1 !== width2
+        
         const maxWidth: number = Math.max(width1, width2);
         const maxHeight: number = Math.max(height1, height2);
 
         const diff: PNG = new PNG({ width: maxWidth, height: maxHeight });
         const ignoredPixelColor: Color = { r: 0, g: 0, b: 255 };
 
-        this.addColoredAreasToImage(img1, excludedAreas, ignoredPixelColor);
-        this.addColoredAreasToImage(img2, excludedAreas, ignoredPixelColor);
+        if (excludedAreas.length > 1) {
+            this.addColoredAreasToImage(img1, excludedAreas, ignoredPixelColor);
+            this.addColoredAreasToImage(img2, excludedAreas, ignoredPixelColor);
+        }
 
-        this.extendImage(img1, maxWidth, maxHeight);
-        this.extendImage(img2, maxWidth, maxHeight);
+        if (imageSizesDoNotMatch) {
+            this.extendImage(img1, maxWidth, maxHeight);
+            this.extendImage(img2, maxWidth, maxHeight);
 
-        this.fillSizeDifference(img1, width1, height1);
-        this.fillSizeDifference(img2, width2, height2);
+            this.fillSizeDifference(img1, width1, height1);
+            this.fillSizeDifference(img2, width2, height2);
+        }
+        let result = 999;
 
-        const result: number = pixelmatch(img1.data, img2.data, diff.data, maxWidth, maxHeight, { threshold: 0.1 });
+        try {
+            result = pixelmatch(img1.data, img2.data, diff.data, maxWidth, maxHeight, { threshold: 0.1 });
+        } catch (error) {console.log(error)}
+
         const isEqual: boolean = result === 0;
 
         if (!isEqual && diffFilePath) {
